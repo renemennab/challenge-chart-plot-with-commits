fc77411aeba36edf582fcef1ffa9e3f9defbccc9
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-11-01 13:38:30 -0300

    start to add button to find out the type of the input

--- src/dataInput.js
+++ src/dataInput.js
@@ -13,7 +13,14 @@ class dataInput extends Component {
 		});
 	};
 
-	logStateHandler = () => console.log(this.state);
+	logStateHandler = () => console.log(this.state.data);
+	extractDataHandler = () => {
+		//JSON.parse(this.state.data)
+		const dt = this.state.data;
+		const obj = JSON.parse(dt);
+		console.log(obj);
+	};
+	//.map(dt => console.log(dt));
 
 	render() {
 		return (
@@ -25,9 +32,21 @@ class dataInput extends Component {
 				/>
 				<p>{JSON.stringify(this.state.data)}</p>
 				<button onClick={this.logStateHandler}>log state</button>
+				<button onClick={this.extractDataHandler}>
+					extract data types
+				</button>
 			</div>
 		);
 	}
 }
 
 export default dataInput;
+
+/* {type: 'start', timestamp: 1519780251293, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']},
+{type: 'span', timestamp: 1519780251293, begin: 1519780251293, end: 1519780260201},
+{type: 'stop', timestamp: 1519780251293},
+{type: 'data', timestamp: 1519780251000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3} */
+
+//{'rene':'2'}
+
+//'{ "name":"John", "age":30, "city":"New York"}'
