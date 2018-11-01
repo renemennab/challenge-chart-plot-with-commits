7c840c00221c8c39812ed048be687db5226e394e
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-11-01 10:55:25 -0300

    add data input to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './header';
+import DataInput from './dataInput';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
+				<DataInput />
 			</div>
 		);
 	}
