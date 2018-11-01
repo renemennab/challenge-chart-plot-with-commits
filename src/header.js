ba3b5585d912c4f6a4cb3c19b2737a93671d7aa9
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-11-01 09:57:51 -0300

    create header component

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const header = () => {
+	return (
+		<header className="header">
+			<p>Renê's Challenge</p>
+		</header>
+	);
+};
+
+export default header;
