515e43a7c3b16d00f9cc3d547d58d69145c9cd70
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-11-03 17:08:16 -0300

    add basic chart

--- src/Components/Chart/chart.js
+++ src/Components/Chart/chart.js
@@ -1,12 +1,41 @@
 import React from 'react';
-//import './chart.css';
+import { Line } from 'react-chartjs-2';
 
 const chart = props => {
 	return (
-		<header className="chart">
-			<p>Renê's Challenge</p>
-		</header>
+		<div className="chart">
+			<Line
+				data={{
+					labels: [0, 0.1],
+					datasets: [
+						{
+							label: 'min chrome mac', // pega a resposta do os e a resposta do browser e junta com um select e transforma em string
+							fill: false,
+							data: [0.1, 1.3], //pega todos os min em uma array
+							borderColor: ['#8768A3'],
+							backgroundColor: ['#8768A3']
+						},
+						{
+							label: 'max chrome mac',
+							data: [0.1, 1.7],
+							fill: false,
+							borderColor: ['#6DBA52'],
+							backgroundColor: ['#6DBA52']
+						}
+					]
+				}}
+				options={{
+					legend: {
+						display: true,
+						position: 'right'
+					},
+					showLines: true
+				}}
+			/>
+		</div>
 	);
 };
 
 export default chart;
+
+//[{ 'min chrome mac': [0.1, 1.3] }];
