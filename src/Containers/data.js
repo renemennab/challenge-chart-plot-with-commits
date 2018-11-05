ddeb8a1a196315ddf102a608f1e6df36d05d9999
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-11-05 09:49:29 -0200

    add all components to data and pass props

--- src/Containers/data.js
+++ src/Containers/data.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header/header';
 import InputIn from '../Components/Input/inputIn';
+import InputOut from '../Components/Input/inputOut';
 import Chart from '../Components/Chart/chart';
 
 class Data extends Component {
+	state = {
+		inputs: [], //array of objects containing all the data from each submit
+		selects: [], //array of strings ['min_response_time', 'max_response_time']
+		groups: [] //array of strings ['os', 'browser']
+	};
+
 	render() {
 		return (
 			<div className="App">
 				<Header />
 				<InputIn />
-				<Chart />
+				<InputOut inputs={this.state.inputs} />
+				<Chart
+					inputs={this.state.inputs} //array of objects containing all the data from each submit
+					selects={this.state.selects} //array of strings ['min_response_time', 'max_response_time']
+					groups={this.state.groups}
+				/>
 			</div>
 		);
 	}
